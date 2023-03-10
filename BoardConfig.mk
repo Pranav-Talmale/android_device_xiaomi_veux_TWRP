984fddba32d63bb3522c5c305f386360edfdde8b
Pranav-Talmale <pranavtalmale@gmail.com>
2023-03-10 17:38:45 +0530

    veux: don't set default language to zh_CN

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,9 +161,6 @@ TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
-ifeq ($(TW_DEFAULT_LANGUAGE),)
-TW_DEFAULT_LANGUAGE := zh_CN
-endif
 TW_DEFAULT_BRIGHTNESS := 200
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
